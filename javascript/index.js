1d0379fa188ab0995730322de879f3c8bad43e32
Alex Le <alex.csm@gmail.com>
2013-08-11 21:54:40 -0600

    major changes to parsing, display, UI, 5 day forecast

---
+++ javascript/index.js
@@ -0,0 +1,60 @@
+function AddIconToId( ID, IconName )
+{
+   var skycons = new Skycons();
+
+   switch (IconName)
+   {
+      case "clear-day":
+         skycons.add(ID, Skycons.CLEAR_DAY);
+         break;
+
+      case "clear-night":
+         skycons.add(ID, Skycons.CLEAR_NIGHT);
+         break;
+
+      case "partly-cloudy-day":
+         skycons.add(ID, Skycons.PARTLY_CLOUDY_DAY);
+         break;
+
+      case "partly-cloudy-night":
+         skycons.add(ID, Skycons.PARTLY_CLOUDY_NIGHT);
+         break;
+
+      case "cloudy":
+         skycons.add(ID, Skycons.CLOUDY);
+         break;
+
+      case "rain":
+         skycons.add(ID, Skycons.RAIN);
+         break;
+
+      case "sleet":
+         skycons.add(ID, Skycons.SLEET);
+         break;
+
+      case "snow":
+         skycons.add(ID, Skycons.SNOW);
+         break;
+
+      case "wind":
+         skycons.add(ID, Skycons.WIND);
+         break;
+
+      case "fog":
+         skycons.add(ID, Skycons.WIFOGND);
+         break;
+   }
+
+   skycons.play();
+}
+
+function LoadSkyCon()
+{
+   var IconIds = ["curr", "day1", "day2", "day3", "day4", "day5"]
+
+   for (i = 0; i < IconIds.length; i++)
+   {
+      AddIconToId( IconIds[i], document.getElementById(IconIds[i]).className )
+   }
+}
+
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os, sys
-import webapp2, jinja2, logging, json
+import webapp2, jinja2, logging, json, datetime
 from google.appengine.api import urlfetch
 
 jinja_environment = jinja2.Environment(
@@ -7,48 +7,97 @@ jinja_environment = jinja2.Environment(
 
 # Get secret and consumer key from data file
 FILE = open('templates/data.txt', 'r')
-CONSUMER_KEY = FILE.readline().strip()
+FORECAST_KEY = FILE.readline().strip()
+BING_KEY = FILE.readline().strip()
+
+CompassDirection = [ 'N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW' ]
 
 class CurrentlyData:
    icon = ''
    temperature = ''
    summary = ''
    wind_speed = ''
+   wind_bearing = ''
    time = '' 
    precip_intensity = '' 
    precip_prob = ''
    humidity = ''
+   cloud_cover = ''
+
+class DayEntry:
+   icon = ''
+   summary = ''
+   time = ''
+   temp_min = ''
+   temp_max = ''
+   precip_prob = ''
+
+class Verdict:
+   status = ''
+   msg = ''
+
+DailyData = []
 
 def GetWeatherData( latitude, longitude ):
-   Data = CurrentlyData()
+   CurrentData = CurrentlyData()
    url = "https://api.forecast.io/forecast/"
    latitude = '39.932318'
    longitude = '-104.985907'
 
-   full_url = url + CONSUMER_KEY + '/' + latitude + ',' + longitude
+   full_url = url + FORECAST_KEY + '/' + latitude + ',' + longitude + '?exclude=alerts,flags'
 
    data = urlfetch.fetch(full_url, deadline=10).content
    json_data = json.loads(data)
 
-   Data.icon = json_data['currently']['icon']
-   Data.temperature = json_data['currently']['temperature']
-   Data.summary = json_data['currently']['summary']
-   Data.wind_speed = json_data['currently']['windSpeed']
-   Data.precip_prob = json_data['currently']['precipProbability']
-   Data.precip_intensity = json_data['currently']['precipIntensity']
-   Data.time = json_data['currently']['time']
-   Data.humidity = json_data['currently']['humidity']
+   # Parse Currently structure from json
+   CurrentData.icon = json_data['currently']['icon']
+   CurrentData.temperature = int(round(json_data['currently']['temperature']))
+   CurrentData.summary = json_data['currently']['summary']
+   CurrentData.wind_speed = json_data['currently']['windSpeed']
+   CurrentData.wind_bearing = CompassDirection[ (int(json_data['currently']['windBearing'] + 180 + 22) % 360) / 45 ]
+   CurrentData.precip_prob = int(round(json_data['currently']['precipProbability'] * 100))
+   CurrentData.precip_intensity = json_data['currently']['precipIntensity']
+   CurrentData.humidity = int(round(json_data['currently']['humidity'] * 100))
+   CurrentData.cloud_cover = int(round(json_data['currently']['cloudCover'] * 100))
+   CurrentData.time = datetime.datetime.fromtimestamp(int(json_data['currently']['time'])).strftime('%a %m-%d %I:%M %p')
 
-   return Data 
-      
+   # Parse Daily structure from json
+   for i in range(5):
+      day_entry = DayEntry()
+
+      day_entry.time = datetime.datetime.fromtimestamp(int(json_data['daily']['data'][i]['time'])).strftime('%a')
+      day_entry.temp_min = int(round(json_data['daily']['data'][i]['temperatureMin']))
+      day_entry.temp_max = int(round(json_data['daily']['data'][i]['temperatureMax']))
+      day_entry.precip_prob= int(round(json_data['daily']['data'][i]['precipProbability'] * 100))
+      day_entry.icon = json_data['daily']['data'][i]['icon']
+      day_entry.summary = json_data['daily']['data'][i]['summary']
+
+      DailyData.append(day_entry)
+   
+   return CurrentData, DailyData
+
+def ShouldAlexRun( CurrentData ):
+   V = Verdict()
+   if CurrentData.temperature < 74:
+      V.status = True
+      V.msg = 'YES'
+   else:
+      V.status = False 
+      V.msg = 'NO, too hot'
+
+   return V
 
 class MainHandler(webapp2.RequestHandler):
    def get(self):
 
-      CurrentData = GetWeatherData(None, None)
+      CurrentData, DailyData = GetWeatherData(None, None)
+
+      Verdict = ShouldAlexRun( CurrentData )
 
       template_values = {
          'CurrentData': CurrentData,
+         'DailyData': DailyData,
+         'Verdict': Verdict,
       }
       
       template = jinja_environment.get_template('index.html')
